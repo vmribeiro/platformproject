a66aad9a5b68436daf7189136b39eac3807762d6
Victor Ribeiro <Victor Ribeiro>
2017-03-11 19:50:02 -0300

    Adição do projeto web, somente a estrutura

---
+++ PlataformaNegociacaoBase/build/generated-sources/ap-source-output/ptf/model/entities/UserLogin_.java
@@ -0,0 +1,17 @@
+package ptf.model.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(UserLogin.class)
+public abstract class UserLogin_ {
+
+	public static volatile SingularAttribute<UserLogin, String> senha;
+	public static volatile SingularAttribute<UserLogin, Integer> tipo;
+	public static volatile SingularAttribute<UserLogin, Long> idLogin;
+	public static volatile SingularAttribute<UserLogin, String> email;
+
+}
+
